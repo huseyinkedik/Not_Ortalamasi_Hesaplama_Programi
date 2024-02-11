aa05e240964f35b4f45c51f5ad51328fc334d0f2
huseyinkedik <159181717+huseyinkedik@users.noreply.github.com>
2024-02-11 22:50:47 +0300

    Eksik yukleme

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Değiskenleri olustur
+        int mat, fizik, turkce, kimya, tarih, muzik;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner inp = new Scanner(System.in);
+
+        //Kullanıcıdan veri al
+
+        System.out.print("Matematik notunu girin:");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik notunu girin:");
+        fizik = inp.nextInt();
+
+        System.out.print("Türkçe notunu girin:");
+        turkce = inp.nextInt();
+
+        System.out.print("Kimya notunu girin:");
+        kimya = inp.nextInt();
+
+        System.out.print("Tarih notunu girin:");
+        tarih = inp.nextInt();
+
+        System.out.print("Müzik notunu girin:");
+        muzik = inp.nextInt();
+
+        double toplam = (mat + fizik + turkce + kimya + tarih + muzik) / 6.0;
+        boolean sonuc = toplam > 60;
+        String str = sonuc ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+
+        System.out.print(str);
+
+
     }
 }
